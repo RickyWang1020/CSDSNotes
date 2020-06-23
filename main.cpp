5e62286d7b9c439eccab3362820f054da75c1acc
Ricky Wang <xw1744@nyu.edu>
2020-06-23 17:11:13 +0800

    updated main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Student_Grade_System
+//
+//  Created by Ricky Lahm Wang on 2020/6/23.
+//  Copyright © 2020 Ricky Lahm Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
