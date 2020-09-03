5f628a91b1a5455c07c384af51b59b7050580a51
RickyWang1020 <50431019+RickyWang1020@users.noreply.github.com>
2020-09-03 16:20:48 +0800

    Add files via upload

---
+++ ElasticSearch/es.py
@@ -0,0 +1,242 @@
+"""
+Function: Testing the basics of elasticsearch using Python
+Author: Xinran Wang
+Date: 09/03/2020
+"""
+
+from elasticsearch import Elasticsearch
+
+
+class ElasticSearchClass:
+
+    def __init__(self, host, port, user, password):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.connect()
+
+    def connect(self):
+        """
+        连接到用户
+        :return: None
+        """
+        self.es = Elasticsearch(hosts=[{'host': self.host, 'port': self.port}], http_auth=(self.user, self.password))
+
+    def create_index(self, index, body):
+        """
+        创建一个新index
+        :param index: 要创建的index名
+        :param body: 数据结构体，用dic表示
+        :return: 添加后的返回信息
+        """
+        try:
+            return self.es.indices.create(index=index, body=body)
+        except Exception as err:
+            print(err)
+
+    def create_document(self, index, body, id_name=None):
+        """
+        新建一条文档到指定的index下;可指定Id,若不指定,ES会自动生成
+        :param index: 待插入的index值
+        :param body: 待插入的数据 -> dict型
+        :param id_name: 自定义Id值
+        :return: 添加后的返回信息（成功与否）
+        """
+        try:
+            return self.es.index(index=index, body=body, id=id_name)
+        except Exception as err:
+            print(err)
+
+    def count(self, index_name):
+        """
+        统计index中包含的文档总数
+        :param index_name:
+        :return: 该index中的文档总数
+        """
+        try:
+            return self.es.count(index=index_name)["count"]
+        except Exception as err:
+            print(err)
+
+    def delete_document(self, index_name, id_name):
+        """
+        删除index中的一个文档
+        :param index_name: index名
+        :param id_name: 文档id名
+        :return: 被删除的文档的内容
+        """
+        try:
+            return self.es.delete(index=index_name, id=id_name)
+        except Exception as err:
+            print(err)
+
+    def delete_index(self, index_name):
+        """
+        删除一条index
+        :param index_name: index名
+        :return: 删除后的返回信息（如果成功：{'acknowledged': True}）
+        """
+        try:
+            return self.es.indices.delete(index_name)
+        except Exception as err:
+            print(err)
+
+    def get(self, index_name, id_name):
+        """
+        获取某一index下的文档的内容
+        :param index_name: index名
+        :param id_name: 文档id名
+        :return: dictionary
+        """
+        try:
+            return self.es.get(index=index_name, id=id_name)
+        except Exception as err:
+            print(err)
+
+    def search(self, index=None, body=None, size=10, scroll='10s'):
+        """
+        查找index/id
+        :param index: 要查的index
+        :param body: 请求体，里面有要搜索的条件
+        :param size: 显示的匹配数据数量上限，不能大于10000
+        :param scroll: 需要保持搜索的上下文环境时间，默认为10秒（类似于保持当前的索引状态快照的时间）
+        :return: 搜索结果dictionary, 其中'hits'键包含搜索到的匹配信息
+        """
+        try:
+            return self.es.search(index=index, body=body, size=size, scroll=scroll)
+        except Exception as err:
+            print(err)
+
+    def scroll(self, scroll_id, scroll):
+        """
+        根据上一个查询方法，查询出来剩下所有相关数据
+        """
+        try:
+            return self.es.scroll(scroll_id=scroll_id, scroll=scroll)
+        except Exception as err:
+            print(err)
+
+    def update(self, index, id_name, body):
+        """
+        更新文档
+        :param index: 要更新的index
+        :param id_name: 文档的id
+        :param body: 要更新的数据内容 -> dict型
+        :return: 更新后的返回信息（成功与否）
+        """
+        try:
+            return self.es.update(index=index, id=id_name, body=body)
+        except Exception as err:
+            print(err)
+
+
+
+if __name__ == "__main__":
+
+    # 连接Elasticsearch
+    obj = ElasticSearchClass('127.0.0.1', '9200', '', '')
+    print(0)
+
+    # 创建一个新的index
+    # 内容长度如果较多使用text，较少使用keyword，一般使用keyword的比较多，因为text查询极其占用内存
+    # 如果添加时间字段建议使用时间戳，类型用keyword
+    mapping = {
+            "mappings": {
+                "properties": {
+                    "email_id": {
+                        "type": "text",
+                        "index": "true"
+                    },
+                    "company_name": {
+                        "type": "keyword",  # keyword不会进行分词,text会分词，integer整数，float浮点数
+                        "index": "true"  # 不建索引
+                    },
+                    "company_id": {
+                        "type": "keyword",  # keyword不会进行分词,text会分词
+                        "index": "true"  # 不建索引
+                    },
+                    "result": {
+                        "type": "object",
+                        "properties": {
+                            "client": {"type": "text", "index": "true"},
+                        }
+                    },
+                    "create_time": {
+                        "type": "keyword",
+                        "index": 'true'
+                    }
+                }
+            }
+        }
+
+    # body = {
+    #     "settings": {
+    #         "number_of_shards": 3,
+    #         "number_of_replicas": 1
+    #     },
+    #     "mappings": {
+    #         "properties": {
+    #             "name": {
+    #                 "type": "text"
+    #             },
+    #             "country": {
+    #                 "type": "keyword"
+    #             },
+    #             "age": {
+    #                 "type": "integer"
+    #             },
+    #             "date": {
+    #                 "type": "date",
+    #                 "format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
+    #             }
+    #         }
+    #     }
+    # }
+    p = obj.create_index(index="testing", body=mapping)
+    print(1)
+
+    # 插入一个文档
+    # obj.create_document(index='test', id_name=16, body={"name": "ricky", "age": 10000})
+    # obj.create_document(index='test', id_name=12, body={"name": "rocky", "age": 111})
+    # obj.create_document(index='testing', id_name=9, body={"name": "jesse", "age": 21323})
+    # print(2)
+
+    # 插入多个测试文档
+    # for i in range(5):
+    #     obj.create_document(index="multi", id_name=i, body={"name": "test name", "age": i})
+    # print(3)
+
+    # 更新文档内容
+    # update_body = {
+    #     "doc": {
+    #         "age": 10,
+    #         "score": "a+"
+    #     }
+    # }
+    # updated = obj.update("test", 16, update_body)
+    # print(4)
+
+    # 查询所有数据
+    all_body = {
+        "query": {
+            "match_all": {}
+        }
+    }
+    all_response = obj.search(index="test", body=all_body)
+    print(all_response)
+    print(5)
+
+    # match：搜索所有包含要检索的内容的文档
+    match_body = {
+        "query": {
+            "match": {
+                "name": "test"
+            }
+        }
+    }
+    match_response = obj.search(body=match_body)
+    print(match_response)
+    print(6)
+
+    # 
