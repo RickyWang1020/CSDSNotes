f871ff6977dc32c4c9bdc08d5e4dbb8c5c4783fc
RickyWang1020 <50431019+RickyWang1020@users.noreply.github.com>
2022-01-26 13:28:40 +0800

    Add files via upload

---
+++ Java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        // int, double, char, bool are primitive variables
+        int hello_hello = 5;
+        double hello_float = 5;
+        String str = "abcde";
+        System.out.println(hello_hello);
+        System.out.println(hello_float);
+        System.out.println(Math.pow(5, 2));
+    }
+}
