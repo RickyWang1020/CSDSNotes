99d103aa98b8d13a6d4ad190c91a1ae5a9bac68e
RickyWang1020 <50431019+RickyWang1020@users.noreply.github.com>
2022-04-09 16:32:56 +0800

    Update and rename Java/Algorithms/dfs_bfs/BfsDfs.java to Java/Algorithms/bfs_dfs/BfsDfs.java

--- Java/Algorithms/bfs_dfs/BfsDfs.java
+++ Java/Algorithms/bfs_dfs/BfsDfs.java
@@ -4,6 +4,8 @@ public void naive_bfs(node) {
     queue.offer(node);
     while (queue 不空) {
         cur = queue.poll();
+        // 对当前节点做操作
+        process_current(cur);
         for (节点 in cur的所有相邻节点) {
             if (该节点有效且未访问过) {
                 queue.offer(该节点);
@@ -21,6 +23,8 @@ public void bfs_level(node) {
         size = queue.size();
         while (size -- > 0) {
             cur = queue.poll();
+            // 对当前节点做操作
+            process_current(cur);
             for (节点 in cur的所有相邻节点) {
                 if (该节点有效且未被访问过) {
                     queue.offer(该节点);
@@ -31,3 +35,16 @@ public void bfs_level(node) {
     }
 }
   
+// DFS
+public void dfs(node, path (此处也可以是visited数组), ...) {
+    if (数据非法或者越界) {return;}
+    if (node满足想要的结果条件) {
+        result.add(包含node的path);
+    }
+    if (可以剪枝) {return;}
+    for (next节点 in node的所有相邻节点) {
+        把node加入path (或者把visited数组对应元素改成true);
+        dfs(next, new_path, ...);
+        把node从path中删除 (或者把visited数组对应元素改成false);
+    }
+}
