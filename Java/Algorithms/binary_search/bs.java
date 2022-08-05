e7df1e261ef11747efa4cbffc92cec5630a6e064
RickyWang1020 <50431019+RickyWang1020@users.noreply.github.com>
2022-08-05 13:12:10 +0800

    Update bs.java

--- Java/Algorithms/binary_search/bs.java
+++ Java/Algorithms/binary_search/bs.java
@@ -1,4 +1,5 @@
 // 模版一：当check(mid)==true调整的是left时，取的是靠左方的答案（floor）
+// 在[left, right)区间内搜索
 long left = 0, right = 1000000;
 while (left < right) {
     long mid = left + right + 1 >> 1;
@@ -12,6 +13,7 @@ while (left < right) {
 
 
 // 模版二：当check(mid)==true调整的是right时，取的是靠右方的答案（ceiling）
+// 在(left, right]区间内搜索
 long left = 0, right = 1000000;
 while (left < right) {
     long mid = left + right >> 1;
@@ -23,11 +25,26 @@ while (left < right) {
     }
 }
 
-// 二分搜索模版：即使left和right相等的时候也可以检查是否搜索到结果
+// 模版三：在[left, right]区间内搜索
+long left = 0, right = 1000000;
+while (left <= right) {
+    long mid = left + (right - left) / 2;
+    if (matrix[mid] < target) {
+        left = mid + 1;
+    }
+    else if (matrix[mid] > target) {
+        right = mid - 1;
+    }
+    else {
+        return mid;
+    }
+}
+
+// 模版四：即使left和right相等的时候也可以检查是否搜索到结果
 long left = 0, right = 1000000;
 while (left < right) {
     long mid = left + right + 1 >> 1;
-    if (matrix[mid] >= target) {
+    if (matrix[mid] <= target) {
         left = mid;
     }
     else {
